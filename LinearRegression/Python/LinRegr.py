a6849a50b9b24a9de31ebea72ed651fa8d5a7b72
sanederchik <makarov.alex.d@gmail.com>
2018-03-09 18:16:14 +0300

    SimpleLinRegression added

---
+++ LinearRegression/Python/LinRegr.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+class LinRegression(object):
+
+    def __init__(self, x, y):
+
+        self.x = np.array(x)
+        self.y = np.array(y)
+
+        assert (len(x) == len(y)) #n_rows is equal
+
+#simple linear regression
+class SimpleLinearRegression(LinRegression):
+
+    def make_regression(self):
+
+        print("Starting program...")
+
+        mean_x = 0
+        mean_y = 0
+        mean_xy = 0
+        mean_x_sq = 0
+
+        for i in np.arange(0, len(self.x), 1):
+
+            mean_x += self.x[i]
+            mean_y += self.y[i]
+            mean_xy += self.x[i] * self.y[i]
+            mean_x_sq += self.x[i]**2
+
+        self.b1 = (mean_xy - mean_x * mean_y) / (mean_x_sq - mean_x**2)
+        self.a = mean_y - self.b1 * mean_x
+
+        print("Equation calculation complete!")
+
+    def report(self):
+
+        print("Equation is %.4f + %.4f * x" % (self.a, self.b1))
+
+A = SimpleLinearRegression(x = np.array([[10], [20], [30]]), y = np.arange(3))
+A.make_regression()
+A.report()
